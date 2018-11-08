1781f5eb8a00cbb913c16ad6dc288f9d874df0c8
Marie Gillier <magillier@hotmail.fr>
2018-11-08 21:39:05 +0100

    cleaned code

--- recipes.js
+++ recipes.js
@@ -23,7 +23,7 @@ const recipeSchema = new Schema(
       type: String,
       default: "https://images.media-allrecipes.com/images/75131.jpg"
     },
-    duration: Number,
+    duration: {type: Number, max: 144},
     creator: String,
     created: { type: Date, default: Date.now }
   },
@@ -35,7 +35,7 @@ const recipeSchema = new Schema(
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
 // ----------------------------------------------------------------------------------------------------------
-// Database connection
+// Database connection and queries
 // ----------------------------------------------------------------------------------------------------------
 
 mongoose
@@ -43,56 +43,17 @@ mongoose
     "mongodb://localhost/recipeApp",
     { useNewUrlParser: true }
   )
-  .then(() => {
-    console.log("Connected to Mongo!");
-  })
-  .catch(err => {
-    console.error("Error connecting to mongo", err);
-  });
+  .then(() => Recipe.insertMany(data))
+  .then(() => Promise.all([updateRigatoni, deleteCarrotCake]))
+  .then(() => mongoose.connection.close())
+  .catch(err => console.error('ERROR IN PROMISE CHAIN', err.message))
 
-// ----------------------------------------------------------------------------------------------------------
-// DB queries
-// ----------------------------------------------------------------------------------------------------------
 
-Recipe.insertMany(data)
-  .then(recipeArr => {
-    console.log(`${recipeArr.length} recipes created!`);
-
-    //once our 5 recipes have been created in the database, then we run our updateRecipeList function,
-    return updateRecipeList();
-  })
-  .then(values => {
-    console.log(values);
-
-    // once our 2 promises inside the updateRecipeList function have resolved, then we close the database
-    mongoose.connection.close(() => console.log("Connection closed"));
-  })
-  .catch(err => console.error(err));
-
-// Promise.all allow us to run several database queries at the same time
-function updateRecipeList() {
   const updateRigatoni = Recipe.findOneAndUpdate(
     { title: { $eq: "Rigatoni alla Genovese" } },
-    { $set: { duration: 100 } }
+    { $set: { duration: 145 } },
+    { runValidators: true, new: true}
   )
-    .then(recipeDoc => {
-      console.log(`${recipeDoc.title} updated`);
-      return recipeDoc;
-    })
-    .catch(err => {
-      console.error("update rigatonni failed");
-      return err;
-    });
 
   const deleteCarrotCake = Recipe.findOneAndDelete({ title: "Carrot Cake" })
-    .then(recipeDoc => {
-      console.log(recipeDoc.title + " removed");
-      return recipeDoc;
-    })
-    .catch(err => {
-      console.error(err);
-      return err;
-    });
 
-  return Promise.all([updateRigatoni, deleteCarrotCake]);
-}
